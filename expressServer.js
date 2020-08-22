cc5cc9c50a2fd9eaece49e202dbbb7a15cd08c0a
김건형 <show11837@gmail.com>
2020-08-22 21:24:59 +0900

    보험데이터 JSON 파일 추가 및 프론트 연결 완료
    + 이미지까지 완료
    + 이미지에 맞는 색상 임의로 설정

--- expressServer.js
+++ expressServer.js
@@ -12,43 +12,28 @@ app.use(express.urlencoded({ extended: false })); // data를 받아 올수 있�
 
 app.use(express.static(__dirname + "/public")); // public이라는 folder를 외부에 공개 하겠다
 
-app.get("/main", function (req, res) {
+app.get("/main", function(req, res) {
     res.render("main");
 });
 
-app.get("/insuranceByCollaborativeFiltering", function (req, res) {
+app.get("/insuranceByCollaborativeFiltering", function(req, res) {
     res.render("insuranceByCollaborativeFiltering");
 });
 
-app.get("/insuranceByKeyword", function (req, res) {
+app.get("/insuranceByKeyword", function(req, res) {
     res.render("insuranceByKeyword");
 });
 
-app.get("/test1", function (req, res) {
-    res.render("test1");
-});
 
-app.get("/test2", function (req, res) {
-    res.render("test2");
-});
-
-app.post("/test2", function (req, res) {
-    // var recommendationResult = collaborativeFiltering.getData();
-    // // res.json(recommendationResult);
-    // console.log(recommendationResult);
+app.post("/insurance", function(req, res) {
     res.json(insuranceJsonFile);
 });
 
-app.post("/insuranceByCollaborativeFiltering", function (req, res) {
+app.post("/insuranceByCollaborativeFiltering", function(req, res) {
     var recommendationResult = collaborativeFiltering.getData();
-    // console.log(recommendationResult);
-    // var data = {
-    //   rank: recommendationResult.indexOf(recommendationResult[0]),
-    //   id: recommendationResult[0]
-    // };
     res.json(recommendationResult);
 });
 
-app.listen(3000, function () {
+app.listen(3000, function() {
     console.log("Example app listening at http://localhost:3000");
 });
